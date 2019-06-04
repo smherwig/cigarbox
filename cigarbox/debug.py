ee0083ac8de46a2fa0e54a86c948d65177a8100e
smherwig <smherwig@cs.umd.edu>
2019-06-04 16:19:12 -0400

    debug: refactor common code

--- cigarbox/debug.py
+++ cigarbox/debug.py
@@ -4,53 +4,53 @@ import sys
 
 DEBUG = False
 
-def debug(s, self=None):
+def _make_line(frame, tag, msg='', self=None):
+    filename = os.path.basename(frame[1])
+    filename, _ = os.path.splitext(filename)
+    lineno = frame[2]
+    funcname = frame[3]
+    if self:
+        funcname = '%s.%s' % (self.__class__.__name__, funcname)
+    msg.rstrip()
+    if msg:
+        line = '%s %s:%d:%s: %s\n' % (tag, filename, lineno, funcname, msg)
+    else:
+        line = '%s %s:%d:%s\n' % (tag, filename, lineno, funcname)
+    return line
+
+def debug(msg, self=None):
     if not DEBUG:
         return
     frames = inspect.stack()
     caller = frames[1]
-    filename = os.path.basename(caller[1])
-    filename, _ = os.path.splitext(filename)
-    lineno = caller[2]
-    funcname = caller[3]
-    if self:
-        funcname = '%s.%s' % (self.__class__.__name__, funcname)
-    s.rstrip()
-    line = '[debug] %s:%d:%s: %s\n' % (filename, lineno, funcname, s)
+    line = _make_line(caller, '[debug]', msg, self)
     sys.stderr.write(line)
 
-def trace_enter(s='', self=None):
-    if not DEBUG:
-        return
+def warn(msg, self=None):
     frames = inspect.stack()
     caller = frames[1]
-    filename = os.path.basename(caller[1])
-    filename, _ = os.path.splitext(filename)
-    lineno = caller[2]
-    funcname = caller[3]
-    if self:
-        funcname = '%s.%s' % (self.__class__.__name__, funcname)
-    line = '[trace] > %s:%d:%s' % (filename, lineno, funcname)
-    if s:
-        s.strip()
-        line += ' %s' % s
-    line += '\n'
+    line = _make_line(caller, '[warn]', msg, self)
     sys.stderr.write(line)
 
-def trace_exit(s='', self=None):
+def die(msg, self=None):
+    frames = inspect.stack()
+    caller = frames[1]
+    line = _make_line(caller, '[die]', msg, self)
+    sys.stderr.write(line)
+    sys.exit(1)
+
+def trace_enter(msg='', self=None):
     if not DEBUG:
         return
     frames = inspect.stack()
     caller = frames[1]
-    filename = os.path.basename(caller[1])
-    filename, _ = os.path.splitext(filename)
-    lineno = caller[2]
-    funcname = caller[3]
-    if self:
-        funcname = '%s.%s' % (self.__class__.__name__, funcname)
-    line = '[trace] < %s:%d:%s' % (filename, lineno, funcname)
-    if s:
-        s.strip()
-        line += ' %s' % s
-    line += '\n'
+    line = _make_line(caller, '[trace >]', msg, self)
+    sys.stderr.write(line)
+
+def trace_exit(msg='', self=None):
+    if not DEBUG:
+        return
+    frames = inspect.stack()
+    caller = frames[1]
+    line = _make_line(caller, '[trace <]', msg, self)
     sys.stderr.write(line)
